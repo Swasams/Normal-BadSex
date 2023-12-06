49a88279bc3b20d5c11c483367b94c40e34d9e66
nyusha-jpg <ai24@nyu.edu>
2023-12-05 19:48:51 -0500

    eliminating errors

--- Normal Bad Sex/Assets/Stories/bar figets/Flower.cs
+++ Normal Bad Sex/Assets/Stories/bar figets/Flower.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
-    public GameObject prefabToInstantiate;
-    //private Animator animator;
-    private SpriteRenderer spriteRenderer;
+    //public GameObject prefabToInstantiate;
+    ////private Animator animator;
+    //private SpriteRenderer spriteRenderer;
     
 
-    void Start()
-    {
-        //animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+    //void Start()
+    //{
+    //    //animator = GetComponent<Animator>();
+    //    spriteRenderer = GetComponent<SpriteRenderer>();
+    //}
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Convert mouse position to world coordinates for 2D
-            Vector2 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Collider2D hit = Physics2D.OverlapPoint(clickPosition);
-            Debug.Log("flower");
-            if (hit != null && hit.gameObject == gameObject)
-            {
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        // Convert mouse position to world coordinates for 2D
+    //        Vector2 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //        Collider2D hit = Physics2D.OverlapPoint(clickPosition);
+    //        Debug.Log("flower");
+    //        if (hit != null && hit.gameObject == gameObject)
+    //        {
                 
 
-                    // Create a new GameObject from the prefab
-                    GameObject newSpriteObject = Instantiate(prefabToInstantiate, clickPosition, Quaternion.identity);
+    //                // Create a new GameObject from the prefab
+    //                GameObject newSpriteObject = Instantiate(prefabToInstantiate, clickPosition, Quaternion.identity);
 
-                    // Trigger the animation
-                    //animator.SetTrigger("flower");
+    //                // Trigger the animation
+    //                //animator.SetTrigger("flower");
                     
-                }
-            }
-        }
+    //            }
+    //        }
+    //    }
     }
 
 
